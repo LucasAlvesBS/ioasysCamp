a72375cffc6f43c1b255a72921a56b6fda129086
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-07 16:15:12 -0300

    Add url

--- semana5/nest-jwt/src/app.module.ts
+++ semana5/nest-jwt/src/app.module.ts
@@ -9,11 +9,7 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: process.env.TYPEORM_CONNECTION,
-      host: process.env.TYPEORM_HOST,
-      port: process.env.TYPEORM_PORT,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
+      url: process.env.DATABASE_URL,
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
       synchronize: true,
     } as TypeOrmModuleOptions),
