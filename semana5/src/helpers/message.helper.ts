c35958fba804044679d98dac1dcf7239ee3cdac6
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-07 17:28:55 -0300

    Update message

--- semana5/src/helpers/message.helper.ts
+++ semana5/src/helpers/message.helper.ts
@@ -2,5 +2,5 @@ export const MessageHelper = {
   PASSWORD_VALID:
     'password must contain uppercase and lowercase letters, numbers and special characters',
   NAME_VALID: 'name must contain only letters',
-  PASSWORD_OR_EMAIL_INVALID: 'invalid credentials',
+  PASSWORD_OR_EMAIL_INVALID: 'invalid credentialss',
 };
