da459d61395a5ca3e2be1bf2a2854264345d3dca
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-07 16:15:41 -0300

    Update PORT

--- semana5/nest-jwt/src/main.ts
+++ semana5/nest-jwt/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
