d7151fc47aa75a753c8ccf56b64d299db4000e58
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-08 07:58:11 -0300

    Remove sql

--- semana5/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE jwt_app;
-
-CREATE TABLE clients (
-    client_uid UUID NOT NULL PRIMARY KEY,
-    client_name VARCHAR(100) NOT NULL,
-    client_email VARCHAR(100) NOT NULL,
-    client_password VARCHAR(255) NOT NULL
-);
