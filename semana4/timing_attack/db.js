1fe39b7242389a09068499fd49da21c626269f15
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-08 20:04:52 -0300

    Update pool

--- semana4/timing_attack/db.js
+++ semana4/timing_attack/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool ({
-    user: 'postgres',
-    password: 'postgres',
-    database: 'login_authentication',
-    host: 'localhost',
-    port: 5432
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 });
 
 module.exports = pool;
